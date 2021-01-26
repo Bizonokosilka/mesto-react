b50b18e73a89ef28ab3af5612612c1d76c0137be
Maxim Alexandrov <89265511699@mail.ru>
2021-01-26 17:39:01 +0300

    0.93

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,23 +1,25 @@
 import React from 'react';
 
-function Card({ card, onCardClick }) {
+function Card(props) {
     const handleClick = () => {
-        onCardClick(card);
+        props.onCardClick(props.card);
     }
 
     return (
         <li className="elements__item">                            
-                <img className="elements__image" src={card.link} alt={card.name} onClick={handleClick} />
+                <img className="elements__image" src={props.card.link} alt={props.card.name} onClick={handleClick} />
                 <button className="elements__trash-btn" type='button'/> 
                 <div className="elements__footer">
-                    <p className="elements__title">{card.name}</p>
+                    <p className="elements__title">{props.card.name}</p>
                     <div className="elements__like-container">  
                         <button className="elements__like-btn" type='button'/>
-                        <span className="elements__like-count">{card.likes.length}</span>
+                        <span className="elements__like-count">{props.card.likes.length}</span>
                     </div> 
                 </div>
             </li>
     );
 }
 
-export default Card;           
+export default Card;        
+
+/* { card, onCardClick } */
